6a593ead66f44490b1fbb7a3949bd631910dd5af
Zülfücan Karakuş <84273117+zulfucankarakus@users.noreply.github.com>
2023-01-22 03:09:09 +0300

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Zülfücan Karakuş on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
